5d3c00336f457146d17edbb839a6efd983c6bc67
ntinos <skiadaskonstantinos8@gmail.com>
2024-02-01 12:27:36 +0200

    Update

---
+++ index-simple.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+array (
+	$section = 	array('title' => 'Πώς ο Σημίτης φέρνει κοντά Μητσοτάκη – Ανδρουλάκη',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Γιάννης Ζουμπουλάκης',
+        'category' => 'Πολιτική'
+    );
+);
+
+foreach ($sections as $x) {
+  echo "$x <br>";
+}
+?>  
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,106 +1,152 @@
-<?php
-$sections = array(
-  array(
-    'id' => 'section1',
-    'title' => 'ΑΦΙΕΡΩΜΑ ΚΩΣΤΑΣ ΣΗΜΙΤΗΣ',
-    'subtitle' => 'ΟΛΑ ΤΑ ΑΡΘΡΑ',
+<!DOCTYPE html>
+<html>
+<body>
+    
+    
+<?php 
 
-    'img' => array(
-      array('img1' => 'src:img/img1.jpg'),
-      array('img2' => 'src:img/img2.jpg'),
-      array('img3' => 'src:img/img3.jpg'),
-      array('img4' => 'src:img/img4.jpg'),
-    )
-    'post-title' => array(
-      array('title1' => 'Πώς ο Σημίτης φέρνει κοντά Μητσοτάκη – Ανδρουλάκη'),
-      array('title2' => 'Εκδήλωση προς τιμήν του πρώην Πρωθυπουργού Κώστα Σημίτη'),
-      array('title3' => 'Πώς ο Σημίτης διαδέχτηκε τον Ανδρέα Παπανδρέου στην πρωθυπουργία το 1996'),
-      array('title4' => 'Η ένταξη της Ελλάδας στο ευρώ ήταν σωστή επιλογή'),
+$sections = array (
+
+$full_article = array('id'=> 'sec-1', 'sectiontitle' => 'ΑΦΙΕΡΩΜΑ ΚΩΣΤΑΣ ΣΗΜΙΤΗΣ','sectionsubtitle' => 'ΟΛΑ ΤΑ ΑΡΘΡΑ', 'layout' => 'layout-1', 'posts' => array(
+	array('title' => 'Πώς ο Σημίτης φέρνει κοντά Μητσοτάκη – Ανδρουλάκη',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Γιάννης Ζουμπουλάκης',
+        'category' => 'Πολιτική'
     ),
-    'themes' => array(
-      array('theme1' => 'Πολιτική'),
-      array('theme2' => 'ΣΥΝΕΝΤΕΥΞΗ'),
+
+	array('title' => 'Εκδήλωση προς τιμήν του πρώην Πρωθυπουργού Κώστα Σημίτη',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Αγγελική Κουρουτζή',
+        'category' => 'Πολιτική'
     ),
-    'name' => array(
-      array('name1' => 'Γιάννης Ζουμπουλάκης'),
-      array('name2' => 'Αγγελική Κουρουτζή'),
-      array('name3' => 'Γιάννης Ζουμπουλάκης'),
-      array('name4' => 'Κώστας Σημίτης'),
-    )
 
-  ),
+	array('title' => 'Πώς ο Σημίτης διαδέχτηκε τον Ανδρέα Παπανδρέου στην πρωθυπουργία το 1996',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Γιάννης Ζουμπουλάκης',
+        'category' => 'Πολιτική'
+    ),
+
+	array('title' => 'Η ένταξη της Ελλάδας στο ευρώ ήταν σωστή επιλογή',
+        'header' => 'ΣΥΝΕΝΤΕΥΞΗ',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Κώστας Σημίτης',
+    ),
+)),
+
+array('id'=> 'sec-2', 'sectiontitle' => 'PODCASTS', 'layout' => 'layout-2', 'posts' => array(
+	array('title' => 'Μέσι: Επίσημος GOAT από τη FIFA',
+        'link' => '#', 
+        'links' => array(
+            array('title' => 'Apple', 'link' => '#'),
+            array('title' => 'Google', 'link' => '#'),
+            array('title' => 'Spotify', 'link' => '#'),
+        ),
+        'image' => 'Cover1.png',
+        'author' => 'BHMA ΣΗΜΕΡΑ',
+    ),
+
+	array('title' => 'Η κρυφή ζωή των λέξεων',
+        'link' => '#',
+        'image' => 'Cover2.png',
+        'author' => 'ΓΛΩΣΣΑ ΓΛΩΣΣΑ',
+        'links' => array(
+            array('title' => 'Apple', 'link' => '#'),
+            array('title' => 'Google', 'link' => '#'),
+            array('title' => 'Spotify', 'link' => '#'),
+        ),
+    ),
+
+	array('title' => 'Ναι, το βιολογικό ρολόι χτυπά και μετά τα 40',
+        'link' => '#',
+        'image' => 'Cover3.png',
+        'author' => 'ΑΠΟ ΠΟΥ ΝΑ ΑΡΧΙΣΩ'
+        'links' => array(
+            array('title' => 'Apple', 'link' => '#'),
+            array('title' => 'Google', 'link' => '#'),
+            array('title' => 'Spotify', 'link' => '#'),
+        ),
+    ),		
+)),
+
+array('id'=> 'sec-3', 'sectiontitle' => 'ΒΗΜΑHISTORY','sectionsubtitle' => 'ΟΛΑ ΤΑ ΑΡΘΡΑ', 'layout' => 'layout-3', 'posts' => array(
+	array('title' => 'Πεδινοί εναντίον Ορεινών: Ο άγνωστος ελληνικός εμφύλιος του 1863',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'date' => '02.12.2022, 06:40',
+        'author' => 'Γιάννης Ζουμπουλάκης',
+        'theme' => 'Πώς ξέσπασε και μεταξύ ποιων πλευρών ο άγνωστος ελληνικός εμφύλιος του 1863'
+    ),
+
+	array('title' => 'Βαλκανικοί Πόλεμοι: Πώς η Ελλάδα διπλασίασε την έκταση και τον πληθυσμό της',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Γιάννης Διαμαντής'
+    ),
+
+	array('title' => 'Τσε Γκεβάρα: Τα στοιχεία που τον έκαναν παγκόσμιο είδωλο',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'author' => 'Γιάννης Διαμαντής'
+    ),		
+)),
 
 
-  array(
-    'id' => 'section2',
-    'title' => 'ΒΗΜΑHISTORY',
-    'subtitle' => 'ΟΛΑ ΤΑ ΑΡΘΡΑ',
-    'posts-title' => 'Πεδινοί εναντίον Ορεινών: Ο άγνωστος ελληνικός εμφύλιος του 1863',
-    'date' => '02.12.2022, 06:40',
-    'name' => 'Γιάννης Διαμαντής'
-    'Γιάννης Διαμαντής',
-    'post-meta' => 'Πώς ξέσπασε και μεταξύ ποιων πλευρών ο άγνωστος ελληνικός εμφύλιος του 1863',
-    'img1' => ,
-    'art-content' => array(
-      array('art1' => 'Βαλκανικοί Πόλεμοι: Πώς η Ελλάδα διπλασίασε την έκταση και τον πληθυσμό της'),
-      array('art2' => 'Τσε Γκεβάρα: Τα στοιχεία που τον έκαναν παγκόσμιο είδωλο'),
-    )
-  ),
+array('id'=> 'sec-4', 'sectiontitle' => '50 ΧΡΟΝΙΑ ΠΟΛΥΤΕΧΝΕΙΟ', 'layout' => 'layout-4', 'posts' => array(
+	array('title' => 'Πολυτεχνείο: Η ιστορία της αιματοβαμμένης σημαίας',
+        'subtitle' => 'Τζουμάκας, Πολάκης, Παπουτσής, Λαζαρίδης, Δραβαλιάρης και πλήθος άλλων κράτησαν τη σημαία του Πολυτεχνείου',
+        'link' => '#',
+        'image' => 'img1.jpg',
+    ),
 
+	array('title' => 'Η Δίκη του Πολυτεχνείου: Οι διαταγές της Χούντας και η απολογία των Δικτατόρων',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'category' => 'Κοινωνία',
+        'author' => 'Δημήτρης Χόνδρος'
+    ),
 
-  array(
-    'id' => 'section3',
-    'title' => 'PODCASTS',
-    'theme' => array(
-      array('theme1' => 'BHMA ΣΗΜΕΡΑ')
-      array('theme2' => 'ΓΛΩΣΣΑ ΓΛΩΣΣΑ')
-      array('theme3' => 'ΑΠΟ ΠΟΥ ΝΑ ΑΡΧΙΣΩ')
-    )
-    'posts-title' => array(
-      array('title1' => 'Μέσι: Επίσημος GOAT από τη FIFA')
-      array('title2' => 'Η κρυφή ζωή των λέξεων')
-      array('title3' => 'Ναι, το βιολογικό ρολόι χτυπά και μετά τα 40')
-    )
-    'button' => array(
+    array('title' => 'Η Δίκη του Πολυτεχνείου: Οι διαταγές της Χούντας και η απολογία των Δικτατόρων',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'category' => 'Πολιτική',
+        'author' => 'Δημήτρης Χόνδρος'
+    ),
 
-    )
+    array('title' => 'Κορυφώνονται οι εκδηλώσεις για την επέτειο του Πολυτεχνείου – Δρακόντεια μέτρα ασφαλείας',
+        'link' => '#',
+        'image' => 'img1.jpg',
+        'category' => 'Κοινωνία',
+        'author' => 'Δημήτρης Χόνδρος'
+    ),
 
-    array(
-      'id' => 'section4',
-      'title' => '50 ΧΡΟΝΙΑ ΠΟΛΥΤΕΧΝΕΙΟ',
-      'posts-title' => array(
-        array('title1' => 'Πολυτεχνείο: Η ιστορία της αιματοβαμμένης σημαίας')
-        array('title2' => 'Η Δίκη του Πολυτεχνείου: Οι διαταγές της Χούντας και η απολογία των Δικτατόρων')
-        array('title3' => 'Στην πορεία Τσίπρας , Κουτσούμπας – Τα μηνύματα')
-        array('title4' => 'Κορυφώνονται οι εκδηλώσεις για την επέτειο του Πολυτεχνείου – Δρακόντεια μέτρα ασφαλείας')
-      )
-      'post-meta' => 'Τζουμάκας, Πολάκης, Παπουτσής, Λαζαρίδης, Δραβαλιάρης και πλήθος άλλων κράτησαν τη σημαία του Πολυτεχνείου',
-      'img' => '',
-      'theme' => array(
-        array('theme1' => 'Κοινωνία')
-        array('theme2' => 'Πολιτική')
-        array('theme3' => 'Κοινωνία')
-      )
-      'name' => array(
-        array('theme1' => 'Κοινωνία')
-        array('theme2' => 'Πολιτική')
-        array('theme3' => 'Κοινωνία')
-      )
-      'ef' => 'ΠΡΩΤΟΣΕΛΙΔΑ ΤΗΣ ΕΠΟΧΗΣ',
-      
-  ),
+    array('title' => 'Κορυφώνονται οι εκδηλώσεις για την επέτειο του Πολυτεχνείου – Δρακόντεια μέτρα ασφαλείας',
+        'link' => '#',
+        'images' => array(
+            array('img' => 'img1.jpg')
+            array('img' => 'img2.jpg')
+        ),
+        'category' => 'Κοινωνία',
+        'author' => 'Δημήτρης Χόνδρος'
+    ),
+
+    array('title' => 'ΠΡΩΤΟΣΕΛΙΔΑ ΤΗΣ ΕΠΟΧΗΣ',
+        'img' => array('sel1.jpg', 'sel2.jpg', 'sel3.jpg', 'playButton.svg'),
+    ),
+)),
+
 ); ?>
 
 
-<?php foreach ($sections as $section) { ?>
-  <div id="<?php echo $section['id']; ?>">
-    <h2>
-      <?php echo $section['title']; ?>
-    </h2>
-    <?php foreach ($section['posts'] as $post) { ?>
-      <article>
-        <?php echo $post['title']; ?>
-      </article>
-    <?php } ?>
-  </div>
-<?php } ?>
+<?php
+    foreach ($full_article as $section) {
+        echo "$section <br>";
+    }    
+?>
+
+
+</body>
+</html>
